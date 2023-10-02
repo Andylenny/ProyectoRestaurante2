2b3e9a99f17f60de6f0d5af848db4184cc9d8608
Andy Lenny Delfino <andydelfino12@hotmail.com>
2023-10-02 12:54:58 -0300

    Producto data (solo la conexion)

--- src/proyectorestaurante/AccesoAdatos/ProductoData.java
+++ src/proyectorestaurante/AccesoAdatos/ProductoData.java
@@ -1,7 +1,19 @@
 
 package proyectorestaurante.AccesoAdatos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import proyectorestaurante.entidades.Producto;
+
 
 public class ProductoData {
+    
+    private Connection conexion;
+
+    public ProductoData(Connection conexion) {
+        this.conexion = conexion;
+    }
+
 
 }
